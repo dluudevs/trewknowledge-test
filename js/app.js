03d766ed1e14e09af0545641aedaaafc68a8ecbb
Derek <derekluu90@gmail.com>
2020-09-30 03:06:45 -0400

    toggle nav menu

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
+const toggleMobile = () => {
+  $('.toggle-mobile--open').click(function() {
+    this.classList.add('inactive')
+    $('.toggle-mobile--close').addClass('active')
+  })
+
+  $('.toggle-mobile--close').click(function() {
+    this.classList.remove('active')
+    $('.toggle-mobile--open').removeClass('inactive')
+  })
+}
+
 $(document).foundation();
+$(document).ready(() => {
+  toggleMobile()
+})
