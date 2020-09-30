73acce230331ef9767046ad7d14f644ec3d04a53
Derek <derekluu90@gmail.com>
2020-09-30 15:02:10 -0400

    completed functionality

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const toggleMobile = () => {
+const toggleMobileMenu = () => {
   $('.toggle-mobile--open').click(function() {
     this.classList.add('inactive')
     $('.toggle-mobile--close').addClass('active')
@@ -10,7 +10,28 @@ const toggleMobile = () => {
   })
 }
 
+const toggleMobileModals = () => {
+  // Language Toggle
+  $('.languages__toggle--mobile').click(() => {
+    $('#languages-mobile').addClass('active')
+    $('html').addClass(['is-reveal-open', 'zf-has-scroll'])
+  })
+  $('#languages-mobile > .close-button').click(() => {
+    $('html').removeClass(['is-reveal-open', 'zf-has-scroll'])
+  })
+  
+  // Countries Toggle
+  $('.country__toggle--mobile').click(() => {
+    $('#country-select').addClass('active')
+    $('html').addClass(['is-reveal-open', 'zf-has-scroll'])
+  })
+  $('#country-select > .close-button').click(() => {
+    $('html').removeClass(['is-reveal-open', 'zf-has-scroll'])
+  })
+}
+
 $(document).foundation();
 $(document).ready(() => {
-  toggleMobile()
+  toggleMobileMenu()
+  toggleMobileModals()
 })
